10334475fc03032f431363fab96d0ca59201dd9a
Your Name <you@example.com>
2018-05-09 18:11:44 +0700

    how-to-markdown

--- elementary-electron/app.js
+++
@@ -1,5 +0,0 @@
-var electron = require('electron');
-electron.app.on('ready',function(){
-    var mainWindow = new electron.BrowserWindow({width: 600, height: 800});
-    mainWindow.loadurl('fileL//'+__dirname+'/index.html');
-});
---
+++ howtomarkdown/test.md
@@ -0,0 +1,8 @@
+
+[NodeSchool Site][ref]
+[GitHub][1]
+[Remark parser]
+
+[ref]: http://www.nodeschool.io
+[1]: https://github.com/
+[Remark parser]: http://remark.js.org/
