5e555e618967be0af625c35199365ef4177208e6
nmaddp1995 <nmaddp199599@gmail.com>
2018-05-16 22:28:30 +0700

    functional

--- functional-javascript/every-some.js
+++ functional-javascript/every-some.js
@@ -1,14 +1,10 @@
-
-
-
 function checkUsersValid(goodUsers){
     return function allUsersValid(submittedUsers){
         return submittedUsers.every(function(user){
             return goodUsers.some(function(user2){
-                return (user2 == user);
+                return (user == user2);
             });
-        })
+        });
     }
 }
-
 module.exports = checkUsersValid; 
---
+++ functional-javascript/test.js
@@ -0,0 +1,5 @@
+var a= [1,2,3,4,4]
+var b= a.some(function(item){
+        return item >1 ;
+    }) 
+console.log(b);
